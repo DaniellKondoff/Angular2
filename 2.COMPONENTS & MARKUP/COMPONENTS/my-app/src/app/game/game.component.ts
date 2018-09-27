22be6c376701edecbf96cdb31a917a5ab572bb1a
Daniel Kondov <danielkondov91@gmail.com>
2018-09-28 00:19:16 +0300

    Component

---
+++ 2.COMPONENTS & MARKUP/COMPONENTS/my-app/src/app/game/game.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Game } from '../domain/game';
+
+@Component({
+  selector: 'app-game',
+  templateUrl: './game.component.html',
+  styleUrls: ['./game.component.css']
+})
+export class GameComponent implements OnInit {
+  games : Array<Game>
+  isShown: boolean = false;
+  constructor() { 
+    this.games = [{id:1, title:"FirstGame", image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNqjIMI4DXakUgs46rwVhhjMFggytM8ivj5euEzFB1kiPauG6J"}, {id:2, title:"SecondsGame", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNqjIMI4DXakUgs46rwVhhjMFggytM8ivj5euEzFB1kiPauG6J"}]
+  }
+
+  ngOnInit() {
+  }
+
+  showContacts(){
+    this.isShown = !this.isShown;
+  }
+}

d7e482635552f58794a5c5571b3d3ca5513f0b95
DKondoff <danielkondov91gmail.com>
2018-10-05 18:47:34 +0300

    last one

--- 3.Services & Dependency Injection/ObservablesAndDI/my-app/src/app/shared/authenticated-route.service.ts
+++ 3.Services & Dependency Injection/ObservablesAndDI/my-app/src/app/shared/authenticated-route.service.ts
@@ -1,8 +1,11 @@
-import { CanActivate } from "@angular/router";
+import { CanActivate, Router } from "@angular/router";
 import { Injectable } from "@angular/core";
 
 @Injectable()
 export class AuthenticatedRoute implements CanActivate {
+    constructor(private router: Router){
+
+    }
     canActivate() {
         return false;
     }
